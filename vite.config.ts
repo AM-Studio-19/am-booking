dd9fc62d107b95efc4ed54351dfdfde0f86b8314
AM-Studio-19 <amylin870712@gmail.com>
2025-12-09 17:00:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-{
-  "compilerOptions": {
-    "composite": true,
-    "skipLibCheck": true,
-    "module": "ESNext",
-    "moduleResolution": "bundler",
-    "allowSyntheticDefaultImports": true
-  },
-  "include": ["vite.config.ts"]
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 關鍵修正：這裡必須跟您的 GitHub Repository 名稱一樣，前後都要有斜線
+  base: '/am-booking/', 
+  build: {
+    outDir: 'dist',
+  }
+});
