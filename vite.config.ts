79d23ddd1d0c36e156eea872526631ac1bdb35ca
AM-Studio-19 <amylin870712@gmail.com>
2025-12-09 17:17:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 關鍵修正：這裡必須跟您的 GitHub Repository 名稱一樣，前後都要有斜線
-  base: '/am-booking/', 
+  base: './', 
   build: {
     outDir: 'dist',
   }
